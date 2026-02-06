3beb9bc897a256ded8891ff76c040de71c1ba4ac
Konstantatou Nefeli <n.konstantatou@megasoft.gr>
2026-02-06 15:38:15 +0200

    Added infrastructure core database context

---
+++ CleanArchitecture.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Domain.Articles;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Article> Articles { get; set; }
+    }
+}
